99a55c17368c421d978372661e0a258345b1a629
AngadSingh04 <singh04angad@gmail.com>
2024-06-19 16:41:55 +0530

    53 Maximum Subarray

---
+++ 53_Maximum_Subarray.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxN = nums[0];
+        int ans = nums[0];
+        int n = nums.size();
+        for(int i = 1; i < n; i++){
+            maxN = max(nums[i], maxN + nums[i]);
+            if(maxN > ans){
+                ans = maxN;
+            }
+        }
+        return ans;
+    }
+};
+
+// you can add this to reduce runtime speed :)
