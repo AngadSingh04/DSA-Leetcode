69765a04c74f9219743c3939203116563dc21bf5
AngadSingh04 <singh04angad@gmail.com>
2024-07-07 22:43:15 +0530

    1863 Sum of all subsets XOR totals

---
+++ 1863_sum_all_totals_xor.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int subsetXORSum(vector<int>& nums) {
+        int n = nums.size();
+        int s = pow(2,n);
+        int ans = 0;
+        for(int i = 0; i < s; i++){
+            int xr = 0;
+            for(int j = 0; j < n; j++){
+                if((1<<j) & i){
+                    xr ^= nums[j];
+                }
+            }
+            ans += xr;
+        }
+        return ans;
+    }
+};
