00438a6f6f5a4bc655c7fb023c41bdcdbd9c71f5
AngadSingh04 <singh04angad@gmail.com>
2024-07-23 22:09:52 +0530

    2810 Fulty keyboard new method uploaded

--- 2810_Faulty_Keyboard.cpp
+++ 2810_Faulty_Keyboard.cpp
@@ -28,3 +28,24 @@ public:
         return ans;
     }
 };
+
+// Second way 
+
+class Solution {
+public:
+    string finalString(string s) {
+        string a = "";
+        for(int i = 0; i < s.length(); i++){
+            if(s[i]!='i'){
+                a += s[i];
+            }else{
+                string b = "";
+                for(int j = a.length() - 1; j >=0; j--){
+                    b += a[j];
+                }
+                a = b;
+            }
+        }
+        return a;
+    }
+};
