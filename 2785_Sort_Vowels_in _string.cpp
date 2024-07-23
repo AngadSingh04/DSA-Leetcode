a2933033a8c67b4dad25a356f10ae01a7b803dde
AngadSingh04 <singh04angad@gmail.com>
2024-07-23 22:53:29 +0530

    2785 Sort Vowels in String

---
+++ 2785_Sort_Vowels_in _string.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isVowel(char c){
+        if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U'){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    string sortVowels(string s) {
+        vector<int> v;
+        for(int i = 0; i < s.length(); i++){
+            if(isVowel(s[i])){
+                v.push_back(s[i]);
+            }
+        }
+        sort(v.begin(),v.end());
+        int cnt = 0;
+        for(int i = 0; i < s.length(); i++){
+            if(isVowel(s[i])){
+                s[i] = v[cnt];
+                cnt++;
+            }
+        }
+        return s;
+    }
+};
