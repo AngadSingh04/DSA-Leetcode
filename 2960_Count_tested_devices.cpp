8107a9bc6a707ae4ed51e827db56aa805c2fc879
AngadSingh04 <singh04angad@gmail.com>
2024-07-27 16:39:12 +0530

    2960 Count tested devices after test operations

---
+++ 2960_Count_tested_devices.cpp
@@ -0,0 +1,16 @@
+lass Solution {
+public:
+    int countTestedDevices(vector<int>& batteryPercentages) {
+        int count = 0;
+        int b = batteryPercentages.size();
+        for(int i = 0; i < b; i++){
+            if(batteryPercentages[i] > 0){
+                count++;
+                for(int j = i+1; j < batteryPercentages.size(); j++){
+                    batteryPercentages[j] = max(0, batteryPercentages[j]-1);
+                }
+            }
+        }
+        return count;
+    }
+};
