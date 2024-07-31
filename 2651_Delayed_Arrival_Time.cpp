5c66eadef607bd5d8621057b6c9ca1b94d182f90
AngadSingh04 <singh04angad@gmail.com>
2024-07-31 21:38:35 +0530

    2651 Calculate delayed arrival time

---
+++ 2651_Delayed_Arrival_Time.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int findDelayedArrivalTime(int arrivalTime, int delayedTime) {
+        int n = arrivalTime + delayedTime;
+        if(n >= 24){
+            return n - 24;
+        }else{
+            return n;
+        }
+    }
+};
