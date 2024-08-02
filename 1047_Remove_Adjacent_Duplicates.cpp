847c55a9d0fa09d2ac229412f4e37f4c4c4c2999
AngadSingh04 <singh04angad@gmail.com>
2024-08-02 16:02:29 +0530

    1047 Remove adjacent duplicates from the string updated

--- 1047_Remove_Adjacent_Duplicates.cpp
+++ 1047_Remove_Adjacent_Duplicates.cpp
@@ -1,3 +1,6 @@
+#include<iostream>
+using namespace std;
+
 class Solution {
 public:
     string removeDuplicates(string s) {
@@ -16,6 +19,22 @@ public:
         //     a.pop();
         // }
         // return ans;
-        
+        int i = 0;
+        while(i < s.length()){
+            if(ans.empty() || ans.back() != s[i]){
+                ans.push_back(s[i]);
+            }else{
+                ans.pop_back();
+            }
+            i++;
+        }
+        return ans;
     }
 };
+int main(){
+    Solution s;
+    string a = "aaabbbccc";
+    string ans = s.removeDuplicates(a);
+    cout << ans<<endl;
+    return 0;
+}
