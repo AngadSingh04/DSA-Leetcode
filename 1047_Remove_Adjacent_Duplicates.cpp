78087293d22e1f3d157641e2296e15f0f1f127e2
AngadSingh04 <singh04angad@gmail.com>
2024-08-02 15:47:03 +0530

    1047 Remove adjacent duplicates from the string

---
+++ 1047_Remove_Adjacent_Duplicates.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        string ans = "";
+        // stack<char> a;
+        // for(char c: s){
+        //     if(!a.empty() && c == a.top()){
+        //         a.pop();
+        //     }else{
+        //         a.push(c);
+        //     }
+        // }
+        // string ans = "";
+        // while(!a.empty()){
+        //     ans = a.top() + ans;
+        //     a.pop();
+        // }
+        // return ans;
+        
+    }
+};
