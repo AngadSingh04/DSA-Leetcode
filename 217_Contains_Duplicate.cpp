46c7804db3110f1e573ee78e3283c16a932f3ddc
AngadSingh04 <singh04angad@gmail.com>
2024-08-14 22:22:48 +0530

    217 Contains Duplicate

---
+++ 217_Contains_Duplicate.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int, int> um;
+        for(int i = 0; i < nums.size(); i++){
+            um[nums[i]]++;
+        }
+        for(int i = 0; i < nums.size(); i++){
+            if(um[nums[i]]>=2){
+                return true;
+            }
+        }
+        return false;
+    }
+};
