7834f2821fd3dd35754f10ddbb9d0bd90f3ff8a6
AngadSingh04 <singh04angad@gmail.com>
2025-02-11 21:45:47 +0530

    1470 Shuffle the array

--- 1470_Shuffle_the_array.cpp
+++ 1470_Shuffle_the_array.cpp
@@ -1,12 +1,11 @@
 class Solution {
-public:
-    vector<int> shuffle(vector<int>& nums, int n) {
-        vector<int> ans;
-        for(int i = 0; i < n; i++){
-            ans.push_back(nums[i]);
-            ans.push_back(nums[n+i]);
+    public:
+        vector<int> shuffle(vector<int>& nums, int n) {
+            vector<int> ans(nums.size());
+            for(int i = 0; i < nums.size()/2; i++){
+                ans.push_back(nums[i]);
+                ans.push_back(nums[nums.size()/2 + i]);
+            }
+            return ans;
         }
-        return ans;
-    }
-};
-//eazyyy
+    };
