63387001643cfe73b26987d69f4b425a56ab5070
AngadSingh04 <singh04angad@gmail.com>
2025-02-14 10:02:30 +0530

    344 Reverse String

--- 344_Reverse_A_String.cpp
+++ 344_Reverse_A_String.cpp
@@ -1,17 +1,12 @@
 class Solution {
-public:
-    void reverseString(vector<char>& s) {
-        string k ="";
-        for(int i=s.size()-1; i>=0;i--){
-            k+=s[i];
+    public:
+        void reverseString(vector<char>& s) {
+            int i = 0;
+            int j = s.size()-1;
+            while(i <= j){
+                swap(s[i], s[j]);
+                i++;
+                j--;
+            }
         }
-        
-        for(int i = 0; i<s.size();i++){
-            s[i] = k[i];
-        }
-
-        for(int i = 0; i<s.size();i++){
-            cout<<s[i];
-        }
-    }
-};
+    };
