598802dfef0feca76f29366343c72216ca9d2fc8
AngadSingh04 <singh04angad@gmail.com>
2025-02-16 19:48:27 +0530

    442 Find all duplicates

--- 442_Find_All_Duplicates.cpp
+++ 442_Find_All_Duplicates.cpp
@@ -1,15 +1,14 @@
 class Solution {
-public:
-    vector<int> findDuplicates(vector<int>& nums) {
-        vector<int> ans;
-        for(int i = 0; i < nums.size(); i++){
-            int a = abs(nums[i]);
-            if(nums[a-1] < 0){
-                ans.push_back(a);
+    public:
+        vector<int> findDuplicates(vector<int>& nums) {
+            vector<int> ans;
+            for(int i = 0; i < nums.size(); i++){
+                int a = abs(nums[i]);
+                if(nums[a - 1] < 0){
+                    ans.push_back(a);
+                }
+                nums[a-1] *= -1;
             }
-            nums[a-1] *= -1;
-            
+            return ans;
         }
-        return ans;
-    }
-};
+    };
