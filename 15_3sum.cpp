cdde9520a129b612aebcb3672452031e4e515a0f
AngadSingh04 <singh04angad@gmail.com>
2025-02-18 21:13:42 +0530

    15 3sum

--- 15_3sum.cpp
+++ 15_3sum.cpp
@@ -1,36 +1,33 @@
-// very good feeling mannn :)
-
 class Solution {
-public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        vector<vector<int>> ans;
-        int n = nums.size();
-        for(int i = 0; i < n-2; i++){
-            if(i > 0 && nums[i] == nums[i-1]){
-                continue;
-            }
-            int low = i+1;
-            int high = n-1;
-            while(low<high){
-                int sum = nums[i] + nums[low] + nums[high];
-                if(sum == 0){
-                    ans.push_back({nums[i],nums[low],nums[high]});
-                    while(low < high && nums[low] == nums[low+1]){
-                        low++;
-                    }
-                    while(high > low && nums[high] == nums[high-1]){
-                        high--;
-                    }
-                    low++;
-                    high--;
-                }else if(sum < 0){
-                    low++;
-                }else{
-                    high--;
+    public:
+        vector<vector<int>> threeSum(vector<int>& nums) {
+            vector<vector<int>> ans;
+            sort(nums.begin(), nums.end());
+            for(int i = 0; i < nums.size(); i++){
+                if(i > 0 && nums[i]==nums[i-1]){
+                    continue;
                 }
-            }    
+                int j = i+1;
+                int k = nums.size()-1;
+    
+                while(j < k){
+                    if(nums[i]+nums[j]+nums[k] == 0){
+                        ans.push_back({nums[i],nums[j],nums[k]});
+                        while(j < k && nums[j] == nums[j+1]){
+                            j++;
+                        }
+                        while(j < k && nums[k] == nums[k-1]){
+                            k--;
+                        }
+                        j++;
+                        k--;
+                    }else if(nums[i]+nums[j]+nums[k] < 0){
+                        j++;
+                    }else{
+                        k--;
+                    }
+                }
+            }
+            return ans;
         }
-        return ans;
-    }
-};
+    };
