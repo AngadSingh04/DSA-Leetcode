b9c967d23b1ff0a9cb15c45a7b133f61f515c53a
AngadSingh04 <singh04angad@gmail.com>
2025-02-23 20:00:03 +0530

    20 Valid Parenthesis

--- 20_Valid_Parentheses.cpp
+++ 20_Valid_Parentheses.cpp
@@ -1,23 +1,24 @@
 class Solution {
-public:
-    bool isValid(string s) {
-        stack<char> mystack;
-        for(int i = 0; i < s.length(); i++){
-            if(s[i] == '(' || s[i] == '{' || s[i] == '['){
-                mystack.push(s[i]);
-            }else{
-                if(mystack.empty() || 
-                s[i]==')' && mystack.top() != '(' ||
-                s[i]==']' && mystack.top() != '[' ||
-                s[i]=='}' && mystack.top() != '{'){
-                    return false;
+    public:
+        bool isValid(string s) {
+            stack<char> mystack;
+            for(int i = 0; i < s.length(); i++){
+                if(s[i] == '(' || s[i] == '{' || s[i] == '['){
+                    mystack.push(s[i]);
+                }else{
+                    
+                    if(mystack.empty() || 
+                    s[i]==')' && mystack.top() != '(' ||
+                    s[i]==']' && mystack.top() != '[' ||
+                    s[i]=='}' && mystack.top() != '{'){
+                        return false;
+                    }
+                    mystack.pop();
                 }
-                mystack.pop();
             }
+            if(mystack.empty()){
+                return true;
+            }
+            return false;
         }
-        if(mystack.empty()){
-            return true;
-        }
-        return false;
-    }
-};
+    };
