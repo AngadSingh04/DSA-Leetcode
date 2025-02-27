c76203db5e085ef9bfca2b1f21306e16ac6aa645
AngadSingh04 <singh04angad@gmail.com>
2025-02-27 21:41:05 +0530

    35 Search Insert Position

--- 35_Search_Insert_Position.cpp
+++ 35_Search_Insert_Position.cpp
@@ -1,11 +1,11 @@
 class Solution {
-public:
-    int searchInsert(vector<int>& nums, int target) {
-        for(int i =0; i < nums.size(); i++){
-            if(nums[i]>= target){
-                return i;
+    public:
+        int searchInsert(vector<int>& nums, int target) {
+            for(int i =0; i < nums.size(); i++){
+                if(nums[i]>= target){
+                    return i;
+                }
             }
+            return nums.size();
         }
-        return nums.size();
-    }
-};
+    };
