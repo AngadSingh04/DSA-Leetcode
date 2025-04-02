d2abdccd0c7c878d314e00b1fd5d105729077d6f
AngadSingh04 <singh04angad@gmail.com>
2025-04-02 16:48:56 +0530

    refactor maximumTripletValue function for improved efficiency and readability

--- 2873_Maximum_ordered_Triplet.cpp
+++ 2873_Maximum_ordered_Triplet.cpp
@@ -1,21 +1,16 @@
 class Solution {
-public:
-    long long maximumTripletValue(vector<int>& nums) {
-        // long long curr = 0;
-        long long max_ans = 0;
-
-        for(int i = 0; i < nums.size()-2; i++){
-            long long a = nums[i];
-            for(int j = i+1; j < nums.size()-1; j++){
-                long long b = nums[j];
-                for(int k =j+1; k < nums.size(); k++){
-                    long long c = nums[k];
-                    if((a-b)*c > max_ans){
-                        max_ans = (a-b)*c;
+    public:
+        long long maximumTripletValue(vector<int>& nums) {
+            long maxi = 0;
+            for (int i = 0 ; i < nums.size() ; i++) {
+                for (int k = nums.size() - 1 ; k > i ; k--) {
+                    int j = i + 1;
+                    while (j < k) {
+                    maxi = max(maxi,(long(nums[i] - nums[j]) * nums[k]));
+                    j++;
                     }
                 }
             }
+            return maxi < 0 ? 0 : maxi;
         }
-        return max_ans;
-    }
-};
+    };
