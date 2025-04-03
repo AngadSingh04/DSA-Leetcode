e1914d0b0c8dc55f982565ae11266e20242126cc
AngadSingh04 <singh04angad@gmail.com>
2025-04-03 22:12:21 +0530

    simplify Fibonacci function using recursion

--- 509_fibonacci_series.cpp
+++ 509_fibonacci_series.cpp
@@ -1,15 +1,12 @@
 class Solution {
-public:
-    int fib(int n) {
-        int r = 0, t0 = 0, t1 = 1;
-        if(n==0 || n==1){
-            return n;
+    public:
+        int fib(int n) {
+            if(n==0){
+                return 0;
+            }
+            if(n==1){
+                return 1;
+            }
+            return fib(n-1)+fib(n-2);
         }
-        for(int i = 2; i<=n;i++){
-            r = t0+t1;
-            t0 = t1;
-            t1 = r;
-        }
-        return r;
-    }
-};
+    };
