511d29c6346da5ee71a801f4e6b417a14eb67025
AngadSingh04 <singh04angad@gmail.com>
2025-04-17 10:02:56 +0530

    add recursive implementation of countPairs function for improved efficiency

--- 2176_Count_equal_divisible.cpp
+++ 2176_Count_equal_divisible.cpp
@@ -13,3 +13,25 @@ class Solution {
             
         }
     };
+
+    //second way 
+
+    class Solution {
+        public:
+            int rcount(vector<int>& nums, int k, int i){
+                if(i >= nums.size()){
+                    return 0;
+                }
+                int count = 0;
+                for(int j = i+1; j < nums.size(); j++){
+                    if(nums[i]==nums[j] && (i*j)%k==0){
+                        count++;
+                    }
+                }
+        
+                return count + rcount(nums, k, i+1);
+            }
+            int countPairs(vector<int>& nums, int k) {
+                return rcount(nums,k, 0);
+            }
+        };
