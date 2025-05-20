3003d7d010c5d43ce4044ebd24bf168c557b0c28
AngadSingh04 <singh04angad@gmail.com>
2025-05-20 07:39:37 +0530

    add implementation of isZeroArray function to determine if the array can be made zero

---
+++ 3355.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
+        int n = nums.size();
+        int q = queries.size();
+
+        vector<int> diffArray(n + 1, 0);
+        for (int i = 0; i < q; i++) {
+            diffArray[queries[i][0]] += 1;
+            diffArray[queries[i][1] + 1] -= 1;
+        }
+
+        int currOperations = 0;
+        for (int i = 0; i < n; i++) {
+            currOperations += diffArray[i];
+            if (currOperations < nums[i]) return false;
+        }
+        return true;
+    }
+};
