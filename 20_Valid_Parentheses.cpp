37f45803a16c344d3972f478b45de26954c59738
AngadSingh04 <singh04angad@gmail.com>
2025-06-03 10:51:52 +0530

    refactor isValid function for improved readability and consistency

--- 20_Valid_Parentheses.cpp
+++ 20_Valid_Parentheses.cpp
@@ -1,24 +1,26 @@
 class Solution {
-    public:
-        bool isValid(string s) {
-            stack<char> s1;
-            for(int i = 0; i < s.length(); i++){
-                if(s[i] == '(' || s[i] == '{' || s[i] == '['){
-                    s1.push(s[i]);
-                }else{
-                    
-                    if(s1.empty() || 
-                    s[i]==')' && s1.top() != '(' ||
-                    s[i]==']' && s1.top() != '[' ||
-                    s[i]=='}' && s1.top() != '{'){
-                        return false;
-                    }
-                    s1.pop();
-                }
-            }
-            if(s1.empty()){
-                return true;
-            }
+public:
+    bool isValid(string s) {
+        stack<char> ss;
+        for(int i = 0; i < s.length(); i++){
+            if(s[i] == '(' ||
+               s[i] == '{' ||
+               s[i] == '[' ){
+                ss.push(s[i]);
+               }else{
+                if( ss.empty() || s[i] == ')' && ss.top()!='(' ||
+                   s[i] == ']' && ss.top()!='[' ||
+                   s[i] == '}' && ss.top()!='{' ){
+                    return false;
+                   }else{
+                    ss.pop();
+                   }
+               }
+        }
+
+        if(!ss.empty()){
             return false;
         }
-    };
+        return true;
+    }
+};
