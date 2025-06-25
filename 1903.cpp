d20bb644196a0d0f8dfbc40695d074297760d384
AngadSingh04 <singh04angad@gmail.com>
2025-06-25 21:07:50 +0530

    refactor largestOddNumber function for clarity and efficiency

--- 1903.cpp
+++ 1903.cpp
@@ -1,13 +1,14 @@
 class Solution {
-    public:
-        string largestOddNumber(string num) {
-            for(int i = num.length()-1; i >= 0; i--){
-                int dig = num[i] - '0';
-                if(dig%2 != 0){
-                    string a = num.substr(0,i+1);
-                    return a;
-                }
+public:
+    string largestOddNumber(string num) {
+        string ans = "";    
+        for(int i = num.size()-1; i >= 0; i--){
+            int digit = num[i] - '0';
+            if(digit % 2 != 0){
+                ans = num.substr(0,i+1);
+                break;
             }
-            return "";
         }
-    };
+        return ans;
+    }
+};
