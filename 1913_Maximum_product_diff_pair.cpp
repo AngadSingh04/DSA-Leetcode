1b88bf111ffce2107c2dec254af9e0fd13e609ea
AngadSingh04 <singh04angad@gmail.com>
2025-06-27 15:56:48 +0530

    refactor maxProductDifference function for improved performance and clarity

--- 1913_Maximum_product_diff_pair.cpp
+++ 1913_Maximum_product_diff_pair.cpp
@@ -1,9 +1,29 @@
 class Solution {
 public:
     int maxProductDifference(vector<int>& nums) {
-        int n = nums.size();
-        sort(nums.begin(),nums.end());
-        int ans = nums[n-1]*nums[n-2] - nums[0]*nums[1];
-        return ans;
+        int la = 0;
+        int sla = 0;
+        int sm = INT_MAX;
+        int ssm = INT_MAX;
+
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] > la){
+                sla = la;
+                la = nums[i];
+            }else{
+                sla = max(sla,nums[i]);
+            }
+        }
+
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] < sm){
+                ssm = sm;
+                sm = nums[i];
+            }else{
+                ssm = min(ssm,nums[i]);
+            }
+        }
+
+        return (la*sla)-(sm*ssm);
     }
 };
