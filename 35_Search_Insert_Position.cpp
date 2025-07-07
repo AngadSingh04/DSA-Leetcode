1717b6dfa2a059b4a0f9e8d334a5ef8f1ad19e80
AngadSingh04 <singh04angad@gmail.com>
2025-07-07 15:16:48 +0530

    refactor searchInsert function to implement binary search for improved efficiency

--- 35_Search_Insert_Position.cpp
+++ 35_Search_Insert_Position.cpp
@@ -1,11 +1,19 @@
 class Solution {
-    public:
-        int searchInsert(vector<int>& nums, int target) {
-            for(int i =0; i < nums.size(); i++){
-                if(nums[i]>= target){
-                    return i;
-                }
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int low = 0;
+        int high = nums.size();
+
+        while(low < high){
+            int mid = low + (high-low)/2;
+            if(nums[mid] == target){
+                return mid;
+            }else if(nums[mid] < target){
+                low= mid+1;
+            }else{
+                high = mid;
             }
-            return nums.size();
         }
-    };
+        return low;
+    }
+};
