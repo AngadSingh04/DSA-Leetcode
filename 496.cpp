e1e157453afb8ebdffac6ad74f9f0299e82f6745
AngadSingh04 <singh04angad@gmail.com>
2025-07-07 22:50:31 +0530

    add implementation of nextGreaterElement function to find the next greater element for each element in nums1 from nums2

---
+++ 496.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        stack<int> st;
+        unordered_map<int,int> um;
+        for(int i = nums2.size()-1; i >= 0; i--){
+            while(!st.empty() && st.top() <= nums2[i]){
+                st.pop();
+            }
+            if(st.empty()){
+                um[nums2[i]] = -1;
+            }else{
+                um[nums2[i]] = st.top();
+            }
+
+            st.push(nums2[i]);
+        }
+        vector<int> vc;
+
+        for(int i = 0; i < nums1.size(); i++){
+            vc.push_back(um[nums1[i]]);
+        }
+        return vc;
+    }
+};
