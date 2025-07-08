0e303c08228d94cf5b8b5859d7872a675db81b56
AngadSingh04 <singh04angad@gmail.com>
2025-07-08 22:28:32 +0530

    refactor rotate function to improve clarity and efficiency by removing unnecessary vector allocation

--- 189_Rotate_array.cpp
+++ 189_Rotate_array.cpp
@@ -1,10 +1,30 @@
 class Solution {
-    public:
-        void rotate(vector<int>& nums, int k) {
-            vector<int> answer(nums.size());
-            for(int i = 0; i < answer.size(); i++){
-                answer[(i+k)%nums.size()]=nums[i];
-            }
-            nums = answer;
+public:
+    void rotate(vector<int>& nums, int k) {
+        // vector<int> vc(nums.size());
+
+        // for(int i = 0; i < nums.size(); i++){
+        //     vc[(i+k)%nums.size()] = nums[i];
+        // }
+
+        // nums = vc;
+
+        k = k%nums.size();
+
+        int i = 0;
+        int j = nums.size()-1;
+        while(i < j){
+            swap(nums[i],nums[j]);
+            i++;
+            j--;
         }
-    };
+
+        for(int i = 0; i < k/2; i++){
+            swap(nums[i], nums[k-1-i]);
+        }
+        for(int i = k, j = nums.size()-1; i < j; j--, i++){
+            swap(nums[i],nums[j]);
+
+        }
+    }
+};
