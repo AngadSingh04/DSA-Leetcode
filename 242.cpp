60d63b245c31d3a12f871ef18546181a5a4f9f13
AngadSingh04 <singh04angad@gmail.com>
2025-07-08 15:49:04 +0530

    refactor isAnagram function for consistency in string length checks and loop formatting

--- 242.cpp
+++ 242.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        unordered_map<char,int> um;
 
-        if(s.size() != t.size()){
+        unordered_map<char,int> um;
+        
+        if(s.length() != t.length()){
             return false;
         }
 
-        for(const auto& p : s){
+        for(const auto& p: s){
             um[p]++;
         }
-        for(const auto& p : t){
+        for(const auto& p: t){
             um[p]--;
         }
 
-        for(const auto& p : um){
+        for(const auto& p: um){
             if(p.second < 0){
                 return false;
             }
         }
+
         return true;
 
     }
