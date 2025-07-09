a458b5b2509a6f09c540e7588592a345ddce0794
AngadSingh04 <singh04angad@gmail.com>
2025-07-09 10:25:48 +0530

    refactor singleNumber function to improve efficiency by eliminating unnecessary sorting and summation logic

--- 136_Single_Number.cpp
+++ 136_Single_Number.cpp
@@ -1,17 +1,11 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int ans;
-        int sum = 0;
+        int xr = 0;
+
         for(int i = 0; i < nums.size(); i++){
-            sum += nums[i];
+            xr ^= nums[i];
         }
-        for(int i = 0; i < nums.size()-1; i++){
-            if(nums[i]==nums[i+1]){
-                sum -= (nums[i]*2);
-            }
-        }
-        return sum;
+        return xr;
     }
 };
