56a968971e1e77d2075b629c6ec5a1020d6510c4
AngadSingh04 <singh04angad@gmail.com>
2025-07-10 10:16:58 +0530

    refactor reverse function to improve readability and maintainability by adding negative number handling and restructuring code

--- 7_Reverse_Integer.cpp
+++ 7_Reverse_Integer.cpp
@@ -1,15 +1,23 @@
 class Solution {
-    public:
-        int reverse(int x) {
-            int ans = 0;
-            while(x != 0){
-                int rem = x%10;
-                if(ans > INT_MAX/10 || ans < INT_MIN/10){
-                    return 0;
-                }
-                ans = (ans*10)+rem;
-                x = x/10;
-            }
-            return ans;
+public:
+    int reverse(int x) {
+        int ans = 0;
+        bool isNeg = false;
+        if(x < 0){
+            isNeg = true;
         }
-    };
+        while(x != 0){
+            int rem = x%10;
+            if(ans > (INT_MAX)/10 || ans < (INT_MIN)/10){
+                return 0;
+            }
+            ans = ans*10 + rem;
+            x = x/10;
+        }
+        // if(isNeg){
+        //     return ans*-1;
+        // }
+
+        return ans;
+    }
+};
