a7cf23ac58f0e845bb11f4b67d0c6ba5bebe09dc
AngadSingh04 <singh04angad@gmail.com>
2025-07-10 16:41:45 +0530

    refactor ListNode definition to convert Java-style syntax to C++ and improve code clarity

--- 876.cpp
+++ 876.cpp
@@ -1,23 +1,26 @@
 /**
  * Definition for singly-linked list.
- * public class ListNode {
+ * struct ListNode {
  *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
  */
 class Solution {
-    public ListNode middleNode(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head;
+public:
+    ListNode* middleNode(ListNode* head) {
 
-        while(fast!=null && fast.next!=null){
-            slow = slow.next;
-            fast = fast.next.next;
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast!=NULL && fast->next!=NULL){
+            slow = slow->next;
+            fast = fast->next->next;
         }
 
         return slow;
+        
     }
-}
+};
