93c200a8f53cdd23baa5b9fb8563bc1a4df1af4b
AngadSingh04 <singh04angad@gmail.com>
2025-07-16 12:34:27 +0530

    refactor countNodes function to improve efficiency using a height-based approach

--- 222.cpp
+++ 222.cpp
@@ -27,3 +27,59 @@ public:
         return count;
     }
 };
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    int checkC(TreeNode* root){
+        if(root == NULL){
+            return 0;
+        }
+        
+        int lh = leftH(root);
+        int rh = rightH(root);
+
+        if(lh == rh){
+            return (1<<lh) -1;
+        }
+
+        return 1+ checkC(root->left) + checkC(root->right);
+    }
+
+    int leftH(TreeNode* root){
+        int l = 0;
+        while(root != NULL){
+            l++;
+            root = root->left;
+        }
+        return l;
+    }
+
+    int rightH(TreeNode* root){
+        int r = 0;
+        while(root != NULL){
+            r++;
+            root = root->right;
+        }
+        return r;
+    }
+
+    
+    
+    int countNodes(TreeNode* root) {
+        int ans = checkC(root);
+        return ans;
+    }
+};
