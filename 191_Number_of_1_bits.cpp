2ac6fbb1c7e638f672dd2347774662dc9dd4b612
AngadSingh04 <singh04angad@gmail.com>
2025-07-17 11:03:52 +0530

    refactor hammingWeight function to improve efficiency by using bitwise operations

--- 191_Number_of_1_bits.cpp
+++ 191_Number_of_1_bits.cpp
@@ -1,12 +1,23 @@
 class Solution {
 public:
     int hammingWeight(int n) {
-    string s = bitset<64>(n).to_string();  
-     int count = 0;
-        for(int i = 0; i < s.length();i++){
-            if(s[i]=='1'){
+        // int i = 0;
+        // int count = 0;
+        // for(int i = 0; i < 32; i++){
+        //     if((n&(1<<i)) != 0){
+        //         count++;
+        //     } 
+        // }
+        // return count;
+
+        int i = 0;
+        int count = 0;
+        while(n!=0){
+            if(n&1 == 1){
                 count++;
             }
+
+            n = n >> 1;
         }
         return count;
     }
