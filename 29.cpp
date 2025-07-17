8818bce49e650a1e6f48f26e8a0b1cd916c2f08b
AngadSingh04 <singh04angad@gmail.com>
2025-07-17 16:56:01 +0530

    refactor divide function for improved readability and consistency

--- 29.cpp
+++ 29.cpp
@@ -4,14 +4,14 @@ public:
         if(dividend == divisor){
             return 1;
         }
-
         bool isNeg = false;
         if(dividend < 0 && divisor > 0 || dividend > 0 && divisor < 0){
             isNeg = true;
         }
 
-        long n = abs(long(dividend));
-        long d = abs(long(divisor));
+        long n = abs(dividend);
+        long d = abs(divisor);
+
         long ans = 0;
 
         while(n >= d){
@@ -19,16 +19,14 @@ public:
             while(n >= (d << (count+1))){
                 count++;
             }
-
             ans += 1 << count;
-            n = n - (d<<count);
-
+            n -= d << count;
         }
-        if(ans == (1<<31)  && isNeg){
-            return INT_MIN;
-         }
         if(ans == (1<<31) && !isNeg){
             return INT_MAX;
+        }
+        if(ans == (1<<31) && isNeg){
+            return INT_MIN;
         }
 
         return isNeg ? ans*-1: ans;
