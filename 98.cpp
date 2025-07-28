4d453cf40216057c919ef22a3bd42e586fc59974
AngadSingh04 <singh04angad@gmail.com>
2025-07-28 19:13:53 +0530

    implement isValidBST function to validate a binary search tree

---
+++ 98.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    bool isValid(TreeNode* root, long minV, long maxV){
+        if(root == NULL){
+            return true;
+        }
+
+        if(root->val <= minV || root->val >= maxV){
+            return false;
+        }
+
+        return isValid(root->left,minV,root->val) && isValid(root->right,root->val,maxV);
+    }
+
+    bool isValidBST(TreeNode* root) {
+
+        long minV = LONG_MIN;
+        long maxV = LONG_MAX;
+
+        return isValid(root,minV,maxV);
+    }
+};
