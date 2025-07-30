f3a4ac43822cb278090fa5a856ee5f79bf0f253f
AngadSingh04 <singh04angad@gmail.com>
2025-07-30 16:06:16 +0530

    refactor: simplify lowestCommonAncestor function by removing findPath and using recursion

--- 236.cpp
+++ 236.cpp
@@ -1,3 +1,58 @@
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+//  * };
+//  */
+// class Solution {
+// public:
+    
+//     bool findPath(vector<TreeNode*>& vc, TreeNode* root, TreeNode* point){
+//         if(root == NULL){
+//             return false;
+//         }
+
+//         vc.push_back(root);
+//         if(root == point){
+//             return true;
+//         }
+
+//         if(findPath(vc,root->left,point) || findPath(vc,root->right, point)){
+//             return true;
+//         }
+
+//         vc.pop_back();
+//         return false;
+//     }
+
+//     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        
+
+//         vector<TreeNode*> vc1;
+//         vector<TreeNode*> vc2;
+
+//         if(!findPath(vc1,root,p) || !findPath(vc2,root,q)){
+//             return NULL;
+//         }
+
+//         int size = min(vc1.size(),vc2.size());
+//         TreeNode* lca = NULL;
+
+//         for(int i = 0; i < size; i++){
+//             if(vc1[i] == vc2[i]){
+//                 lca = vc1[i];
+//             }
+//         }
+
+//         return lca;
+
+
+//     }
+// };
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -10,45 +65,22 @@
 class Solution {
 public:
     
-    bool findPath(vector<TreeNode*>& vc, TreeNode* root, TreeNode* point){
-        if(root == NULL){
-            return false;
-        }
-
-        vc.push_back(root);
-        if(root == point){
-            return true;
-        }
-
-        if(findPath(vc,root->left,point) || findPath(vc,root->right, point)){
-            return true;
-        }
-
-        vc.pop_back();
-        return false;
-    }
 
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         
-
-        vector<TreeNode*> vc1;
-        vector<TreeNode*> vc2;
-
-        if(!findPath(vc1,root,p) || !findPath(vc2,root,q)){
-            return NULL;
+        if(root == NULL || root == p || root == q){
+            return root;
         }
 
-        int size = min(vc1.size(),vc2.size());
-        TreeNode* lca = NULL;
+        TreeNode* left = lowestCommonAncestor(root->left,p,q);
+        TreeNode* right = lowestCommonAncestor(root->right,p,q);
 
-        for(int i = 0; i < size; i++){
-            if(vc1[i] == vc2[i]){
-                lca = vc1[i];
-            }
+        if(left == NULL){
+            return right;
+        }else if(right == NULL){
+            return left;
+        }else{
+            return root;
         }
-
-        return lca;
-
-
     }
 };
