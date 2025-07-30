ab893ff61129baca48fb7fb65d931da3c1e67c77
AngadSingh04 <singh04angad@gmail.com>
2025-07-30 16:58:47 +0530

    implement bstFromPreorder function to construct a binary search tree from preorder traversal

---
+++ 1008.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    TreeNode* createTree(vector<int>& preorder, int start, int end){
+        if(start > end){
+            return NULL;
+        }
+        TreeNode* root = new TreeNode(preorder[start]);
+        int rightStart = start+1;
+        while(rightStart <= end && preorder[rightStart] < preorder[start]){
+            rightStart++;
+        }
+
+        root->left = createTree(preorder,start+1,rightStart-1);
+        root->right = createTree(preorder,rightStart, end);
+
+        return root;
+    }
+    TreeNode* bstFromPreorder(vector<int>& preorder) {
+
+        if(preorder.size() == 0){
+            return NULL;
+        }
+        return createTree(preorder,0,preorder.size()-1);
+        
+    }
+};
