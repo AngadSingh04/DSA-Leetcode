4f5a3de1cc46adc00f410012f41e9c120631d598
AngadSingh04 <singh04angad@gmail.com>
2025-07-31 15:10:10 +0530

    refactor jump function to remove commented code and improve readability

--- 45.cpp
+++ 45.cpp
@@ -4,22 +4,18 @@ public:
         int jumps = 0;
         int l = 0;
         int r = 0;
-        // 0,1,2,3,4
-        // 2,3,1,1,4
-        //   l
-        //     r
 
         while(r < nums.size()-1){
             int range = 0;
             for(int i = l; i <= r; i++){
                 range = max(range, i+nums[i]);
             }
-            l = r+1; // 1
-            r = range; // 2
+            l = r+1;
+            r = range;
             jumps++;
         }
 
-    return jumps;
+        return jumps;
         
     }
 };
