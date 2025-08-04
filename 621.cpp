95e6bd0a84b562bb079023ba9b8c3fd24965be58
AngadSingh04 <singh04angad@gmail.com>
2025-08-04 16:14:01 +0530

    implement leastInterval function to calculate the least number of intervals required to complete all tasks with cooling time

---
+++ 621.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        
+        vector<int> vc(26,0);
+        priority_queue<int> pq;
+
+        for(int i = 0; i < tasks.size();i++){
+            vc[tasks[i] - 'A']++;
+        }
+
+        for(int i = 0; i < vc.size(); i++){
+            if(vc[i] > 0){
+                pq.push(vc[i]);
+            }
+        }
+
+        int count = 0;
+
+        while(!pq.empty()){
+            vector<int> temp;
+
+            for(int i = 1; i <= n+1; i++){
+                if(!pq.empty()){
+                    int freq = pq.top();
+                    pq.pop();
+                    freq--;
+                    temp.push_back(freq);
+                }
+            }
+
+            for(int i = 0; i < temp.size(); i++){
+                if(temp[i] > 0){
+                    pq.push(temp[i]);
+                }
+            }
+
+            if(!pq.empty()){
+                count += n+1;
+            }else{
+                count += temp.size();
+            }
+        }
+
+        return count;
+    }
+};
