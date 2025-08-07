d1f1db15e899d34c2382a16a5bdb1ae9c1612278
AngadSingh04 <singh04angad@gmail.com>
2025-08-07 19:49:04 +0530

    implement canFinish function to determine if all courses can be completed given prerequisites

---
+++ 207.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    void bfss(queue<int>& q, vector<int>& inDeg, vector<vector<int>>& adj, vector<int>& ans){
+        
+        while(!q.empty()){
+            int tmp = q.front();
+            q.pop();
+            ans.push_back(tmp);
+            
+            for(const auto& p: adj[tmp]){
+                inDeg[p]--;
+                if(inDeg[p]==0){
+                    q.push(p);
+                }
+            }
+        }
+    }
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses);
+        
+        for(const auto& p: prerequisites){
+            adj[p[1]].push_back(p[0]);
+        }
+        
+        
+        vector<int> inDeg(numCourses,0);
+        
+        for(int i = 0; i < numCourses; i++){
+            for(const auto& p: adj[i]){
+                inDeg[p]++;
+            }
+        }
+        
+        queue<int> q;
+        for(int i = 0; i < numCourses; i++){
+            if(inDeg[i] == 0){
+                q.push(i);
+            }
+        }
+        
+        vector<int> ans;
+        bfss(q,inDeg,adj,ans);
+        if(ans.size()!=numCourses){
+            return false;
+        }
+        return true;
+
+        
+    }
+};
