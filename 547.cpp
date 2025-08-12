8b85ab11de3aad89ba5f7d1ef7ef0395a070c3ab
AngadSingh04 <singh04angad@gmail.com>
2025-08-12 19:32:02 +0530

    implement findCircleNum function using Disjoint Set to count connected components in a graph

--- 547.cpp
+++ 547.cpp
@@ -35,3 +35,91 @@ public:
         return count;
     }
 };
+
+
+class Solution {
+    class DisjointSet{
+    vector<int> rank, parent,size;
+    public:
+    DisjointSet(int n){
+        rank.resize(n+1,0);
+        parent.resize(n+1);
+        size.resize(n+1,1);
+        for(int i = 0; i <= n; i++){
+            parent[i] = i;
+        }
+    }
+
+    int findUp(int node){
+        if(node == parent[node]){
+            return node;
+        }
+        return parent[node] = findUp(parent[node]);
+    }
+
+    void unionByRank(int u, int v){
+        int up_u = findUp(u);
+        int up_v = findUp(v);
+
+        if(up_u == up_v){
+            return;
+        }
+        if(rank[up_u] < rank[up_v]){
+            parent[up_u] = up_v;
+        }else if(rank[up_v] < rank[up_u]){
+            parent[up_v] = up_u;
+        }else{
+            parent[up_v] = up_u;
+            rank[up_u]++;
+        }
+    }
+
+    void unionBySize(int u, int v){
+        int up_u = findUp(u);
+        int up_v = findUp(v);
+        if(up_u == up_v){
+            return;
+        }
+        if(rank[up_u] < rank[up_v]){
+            parent[up_u] = up_v;
+            size[up_v] += size[up_u];
+        }else{
+            parent[up_v] = up_u;
+            size[up_u] += size[up_v];
+        }
+    }
+}; 
+public:
+
+    // void dfs(int start, vector<vector<int>>& adj, vector<int>& visited){
+    //     visited[start] = 1;
+
+    //     for(const auto& a: adj[start]){
+    //         if(visited[a] == 0){
+    //             dfs(a,adj,visited);
+    //         }
+    //     }
+    // }
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int c = isConnected.size();
+        
+    DisjointSet ds(c);
+
+    for(int i = 0; i < c; i++){
+        for(int j = 0; j < c; j++){
+            if(isConnected[i][j] == 1){
+                ds.unionBySize(i+1,j+1);
+            }
+        }
+    }
+    int count = 0;
+    for(int i = 1; i <= c; i++){
+        if(i == ds.findUp(i)){
+            count++;
+        }
+    }
+
+    return count;
+
+    }
+};
