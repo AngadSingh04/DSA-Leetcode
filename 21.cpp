4eb529a59584b340430196b6fc3e72170b3642bf
AngadSingh04 <singh04angad@gmail.com>
2025-08-16 15:14:22 +0530

    practice

--- 21.cpp
+++ 21.cpp
@@ -51,3 +51,44 @@ class Solution {
 
     }
 }
+
+//
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* ansNode = new ListNode(-1);
+        ListNode* tmp = ansNode;
+
+        while(list1!=NULL && list2!=NULL){
+            if(list1->val < list2->val){
+                tmp->next = list1;
+                tmp = list1;
+                list1 = list1->next;
+
+            }else{
+                tmp->next = list2;
+                tmp = list2;
+                list2 = list2->next;
+            }
+        }
+
+        if(list1!=NULL){
+            tmp->next = list1;
+        }
+        if(list2!=NULL){
+            tmp->next = list2;
+        }
+
+        return ansNode->next;
+    }
+};
