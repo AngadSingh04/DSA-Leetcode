9af7b81dc8316f6fada258682b39a6e2e82666b8
AngadSingh04 <singh04angad@gmail.com>
2025-08-21 15:57:16 +0530

    refactor rotate method for improved readability and efficiency

--- 189_Rotate_array.cpp
+++ 189_Rotate_array.cpp
@@ -1,30 +1,13 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        // vector<int> vc(nums.size());
+        
+        int n = nums.size();
+        k = k%n;
 
-        // for(int i = 0; i < nums.size(); i++){
-        //     vc[(i+k)%nums.size()] = nums[i];
-        // }
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin()+k);
+        reverse(nums.begin() + k, nums.end());
 
-        // nums = vc;
-
-        k = k%nums.size();
-
-        int i = 0;
-        int j = nums.size()-1;
-        while(i < j){
-            swap(nums[i],nums[j]);
-            i++;
-            j--;
-        }
-
-        for(int i = 0; i < k/2; i++){
-            swap(nums[i], nums[k-1-i]);
-        }
-        for(int i = k, j = nums.size()-1; i < j; j--, i++){
-            swap(nums[i],nums[j]);
-
-        }
     }
 };
