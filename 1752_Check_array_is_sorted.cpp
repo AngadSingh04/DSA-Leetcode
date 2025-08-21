e1195a725501a2359153c0518f6604242d092d30
AngadSingh04 <singh04angad@gmail.com>
2025-08-21 12:57:58 +0530

    refactor check method for improved readability and maintainability

--- 1752_Check_array_is_sorted.cpp
+++ 1752_Check_array_is_sorted.cpp
@@ -1,18 +1,24 @@
 class Solution {
-    public:
-        bool check(vector<int>& nums) {
-            int count = 0;
-            for(int i = 1; i < nums.size(); i++){
-                if(nums[i-1] > nums[i]){
-                    count++;
-                }
-            }
-            if(nums[nums.size()-1] > nums[0]){
+public:
+    bool check(vector<int>& nums) {
+        int count = 0;
+
+        // 2 5 7 4 3
+        // 2 3 4 5 1
+
+        for(int i = 1; i < nums.size(); i++){
+            if(nums[i-1] > nums[i]){
                 count++;
             }
-            if(count > 1){
-                return false;
-            }
-            return true;
         }
-    };
+
+        if(nums[nums.size()-1] > nums[0]){
+            count++;
+        }
+
+        if(count > 1){
+            return false;
+        }
+        return true;
+    }
+};
