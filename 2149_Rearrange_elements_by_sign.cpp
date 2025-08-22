b7a3d2ea7b17768cd95d48932a2ec838812ae987
AngadSingh04 <singh04angad@gmail.com>
2025-08-22 19:30:45 +0530

    refactor rearrangeArray method for clarity and correct logic flow

--- 2149_Rearrange_elements_by_sign.cpp
+++ 2149_Rearrange_elements_by_sign.cpp
@@ -4,16 +4,16 @@ public:
         vector<int> ans(nums.size(),0);
         int pos = 0;
         int neg = 1;
-
-        for(int i = 0;i < nums.size(); i++){
-            if(nums[i] > 0){
-                ans[pos] = nums[i];
-                pos+=2;
-            }else{
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] < 0){
                 ans[neg] = nums[i];
-                neg+=2;
+                neg = neg+2;
+            }else{
+                ans[pos] = nums[i];
+                pos = pos+2;
             }
         }
+
         return ans;
     }
 };
