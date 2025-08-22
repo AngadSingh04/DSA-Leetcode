8f8a4a492c98af81de2da6d3fb65cd4451400552
AngadSingh04 <singh04angad@gmail.com>
2025-08-22 14:14:25 +0530

    practice

--- 75.cpp
+++ 75.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int i = 0;
+        int low = 0;
         int mid = 0;
         int high = nums.size()-1;
 
         while(mid <= high){
             if(nums[mid] == 0){
-                swap(nums[i],nums[mid]);
-                i++;
+                swap(nums[low],nums[mid]);
+                low++;
                 mid++;
             }else if(nums[mid] == 1){
                 mid++;
@@ -17,5 +17,6 @@ public:
                 high--;
             }
         }
+
     }
 };
