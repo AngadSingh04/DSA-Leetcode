2724b6320edc5d52b9bdae1ad75dfdf03e2dbeb6
AngadSingh04 <singh04angad@gmail.com>
2025-08-24 10:28:55 +0530

    practice

--- 547.cpp
+++ 547.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
-
-    void dfs(int start, vector<vector<int>>& adj, vector<int>& visited){
-        visited[start] = 1;
-
-        for(const auto& a: adj[start]){
-            if(visited[a] == 0){
-                dfs(a,adj,visited);
+    void dfss(int start, vector<vector<int>>& adj, vector<int>& vis){
+        vis[start] = 1;
+        for(const auto& p: adj[start]){
+            if(vis[p] == 0){
+                dfss(p,adj,vis);
             }
         }
     }
     int findCircleNum(vector<vector<int>>& isConnected) {
         int c = isConnected.size();
         vector<vector<int>> adj(c+1);
-        int count = 0;
 
-        for(int i = 0; i < isConnected.size(); i++){
-            for(int j = 0; j < isConnected.size(); j++){
+        for(int i = 0; i < c; i++){
+            for(int j = 0; j < c; j++){
                 if(isConnected[i][j] == 1 && i!=j){
                     adj[i+1].push_back(j+1);
                     adj[j+1].push_back(i+1);
@@ -24,15 +21,16 @@ public:
             }
         }
 
-        vector<int> visited(c+1,0);
-
-        for(int i = 1; i <= isConnected.size(); i++){
-            if(visited[i] == 0){
-                dfs(i,adj,visited);
+        vector<int> vis(c+1, 0);
+        int count = 0;
+        for(int i = 0; i < vis.size(); i++){
+            if(vis[i] == 0){
+                dfss(i, adj, vis);
                 count++;
             }
         }
-        return count;
+
+        return count-1;
     }
 };
 
