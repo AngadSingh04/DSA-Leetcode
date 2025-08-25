2e6f6957d8b6882fa9ba4a5696d51a40ba7ac2e3
AngadSingh04 <singh04angad@gmail.com>
2025-08-25 21:42:16 +0530

    refactor search function in 33_Search_in_rotated_sorted_array to simplify logic and improve readability

--- 33_Search_in_rotated_sorted_array.cpp
+++ 33_Search_in_rotated_sorted_array.cpp
@@ -1,41 +1,30 @@
 class Solution {
 public:
-    int getPoint(vector<int>& nums){
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
         int low = 0;
-        int high = nums.size()-1;
+        int high = n-1;
 
-        while(low < high){
-            int mid = low + (high-low)/2;
-            if(nums[mid] >= nums[0]){
-                low = mid+1;
-            }else{
-                high=mid;
-            }
-        }
-        return low;
-    }
-    int bS(vector<int>& nums, int low, int high, int target){
-        int l = low;
-        int h = high;
-
-        while(l <= h){
-            int mid = l + (h-l)/2;
+        while(low <= high){
+            int mid = low + (high - low)/2;
             if(nums[mid] == target){
                 return mid;
-            }else if(nums[mid] < target){
-                l = mid+1;
+            }
+            if(nums[low] <= nums[mid]){
+                if(nums[low] <= target && target <= nums[mid]){
+                    high = mid-1;
+                }else{
+                    low = mid+1;
+                }
             }else{
-                h = mid-1;
+                if(nums[mid] <= target && target <= nums[high]){
+                    low = mid+1;
+                }else{
+                    high = mid-1;
+                }
             }
         }
+
         return -1;
-    }
-    int search(vector<int>& nums, int target) {
-        int point = getPoint(nums);
-        if(target >= nums[point] && target <= nums[nums.size()-1]){
-            return bS(nums, point, nums.size()-1,target);
-        }else{
-            return bS(nums, 0, point, target);
-        }
     }
 };
