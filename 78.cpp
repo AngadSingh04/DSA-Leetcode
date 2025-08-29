25fd868ebe00b43aff788e44cd590b91ab47e3fb
AngadSingh04 <singh04angad@gmail.com>
2025-08-29 18:13:27 +0530

    implement subsets function to generate all possible subsets of a given set

---
+++ 78.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        int n = nums.size();
+        int size = (1 << n);
+
+        for(int i = 0; i < size; i++){
+            vector<int> tmp;
+            for(int j = 0; j < n; j++){
+                if(i & (1 << j)){
+                    tmp.push_back(nums[j]);
+                }
+            }
+            ans.push_back(tmp);
+        }
+
+        return ans;
+    }
+};
