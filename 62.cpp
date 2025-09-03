1ec9a673c75dcf28eacf0b88c74943ffcfcf8fc7
AngadSingh04 <singh04angad@gmail.com>
2025-09-03 14:02:51 +0530

    implement tabulation method for uniquePaths function to optimize path calculation

--- 62.cpp
+++ 62.cpp
@@ -23,3 +23,34 @@ public:
         return getWays(m-1,n-1, dp);
     }
 };
+
+// tabulation
+class Solution {
+public:
+    int getWays(int m, int n, vector<vector<int>>& dp){
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                int top = 0;
+                int left = 0;
+                if(i == 0 && j == 0){
+                    dp[0][0] = 1;
+                }else{
+                    if(i > 0){
+                        top = dp[i-1][j];
+                    }
+                    if(j > 0){
+                        left = dp[i][j-1];
+                    }
+                    dp[i][j] = top + left;
+                }
+            }
+        }
+
+        return dp[m-1][n-1];
+    }
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return getWays(m,n, dp);
+    }
+};
