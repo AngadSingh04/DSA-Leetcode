f699f5995d41cef72b30e141abaec308036ca57d
AngadSingh04 <singh04angad@gmail.com>
2025-09-06 13:03:06 +0530

    implement tabulation approach for coinChange function to optimize minimum coin count calculation

--- 322.cpp
+++ 322.cpp
@@ -37,3 +37,51 @@ public:
         return ans;
     }
 };
+
+// tabulation
+
+class Solution {
+public:
+    int getCount(int ind, vector<int>& coins, int amount, vector<vector<int>>& dp){
+        for(int i = 0; i <= ind; i++){
+            dp[i][0] = 0;
+        }
+        for(int i = 1; i <= amount; i++){
+            if(i % coins[0] == 0){
+                dp[0][i] = i / coins[0];
+            }else{
+                dp[0][i] = INT_MAX;
+            }
+        }
+        for(int i = 1; i <= ind; i++){
+            for(int j = 1; j <= amount; j++){
+                int pick = INT_MAX;
+                if(coins[i] <= j){
+                    int tmp = dp[i][j - coins[i]];
+                    if(tmp != INT_MAX){
+                        pick = 1 + tmp;
+                    }
+                }
+                int npick = dp[i-1][j];
+
+                dp[i][j] = min(pick,npick);
+            }
+        }
+        return dp[ind][amount];
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        // sort(coins.begin(),coins.end());
+        int ind = n-1;
+        if(amount == 0){
+            return 0;
+        }
+
+        vector<vector<int>> dp(n, vector<int>(amount+1, 0));
+        int ans =  getCount(ind, coins, amount,dp);
+        if(ans == INT_MAX){
+            return -1;
+        }
+        return ans;
+    }
+};
