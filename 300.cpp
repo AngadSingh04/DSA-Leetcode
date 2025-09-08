0fc56c9a88a52279a80eb69dc19313a124bc7635
AngadSingh04 <singh04angad@gmail.com>
2025-09-08 21:29:51 +0530

    binary search on lis

--- 300.cpp
+++ 300.cpp
@@ -55,3 +55,25 @@ public:
         return maxi;
     }
 };
+
+// more optimal version for getting count
+
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& arr) {
+        int n =arr.size();
+        vector<int> temp;
+        temp.push_back(arr[0]);
+
+        for(int i = 0; i < n; i++){
+            if(arr[i] > temp.back()){
+                temp.push_back(arr[i]);
+            }else{
+                int ind = lower_bound(temp.begin(), temp.end(), arr[i]) - temp.begin();
+                temp[ind] = arr[i];
+            }
+        }
+
+        return temp.size();
+    }
+};
