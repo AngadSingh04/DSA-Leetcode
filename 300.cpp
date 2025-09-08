1c216ccf47088e1e91e796f8c4a2b49cb87b1a37
AngadSingh04 <singh04angad@gmail.com>
2025-09-08 18:14:24 +0530

    implement lengthOfLIS function to calculate the length of the longest increasing subsequence in an array using dynamic programming

---
+++ 300.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    int getMax(int ind, int prev, int n, vector<int>& arr, 
+    vector<int>& front, vector<int>& curr){
+
+        for(int i = n-1; i >= 0; i--){
+            for(int j = i-1; j >= -1; j--){
+                int take = 0;
+                if(j == -1 || arr[i] > arr[j]){
+                    take = 1 + front[i+1];
+                }
+                int ntake = front[j+1];
+                curr[j+1] = max(take,ntake);
+            }
+            front = curr;
+        }
+        
+        return front[0];
+    }
+    int lengthOfLIS(vector<int>& arr) {
+        int n = arr.size();
+        int prev = -1;
+        vector<int> front(n+1, 0);
+        vector<int> curr(n+1, 0);
+        
+        return getMax(0, prev, n, arr, front, curr);
+    }
+};
+
+//optimal
+
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> dp(n , 1);
+
+        // 5 4 11 1 16 8
+        // 5 4
+
+        for(int i = 0; i < n; i++){
+            int tmp = 1;
+            for(int j = 0; j < i; j++){
+                if(arr[j] < arr[i]){
+                    tmp = max(tmp, dp[i] + dp[j]);
+                }
+            }
+            dp[i] = tmp;
+        }
+
+        int maxi = 0;
+        for(int i = 0; i < n; i++){
+            maxi = max(maxi, dp[i]);
+        }
+        return maxi;
+    }
+};
