2e300946413e2790e4d88057871694aa0765f2f3
AngadSingh04 <singh04angad@gmail.com>
2025-09-10 14:00:43 +0530

    practice

--- 300.cpp
+++ 300.cpp
@@ -60,20 +60,21 @@ public:
 
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& arr) {
-        int n =arr.size();
-        vector<int> temp;
-        temp.push_back(arr[0]);
+    int lengthOfLIS(vector<int>& arr){
+        int n = arr.size();
+        vector<int> tmp;
+        tmp.push_back(arr[0]);
 
-        for(int i = 0; i < n; i++){
-            if(arr[i] > temp.back()){
-                temp.push_back(arr[i]);
+        for(int i = 1; i < n; i++){
+            if(arr[i] > tmp.back()){
+                tmp.push_back(arr[i]);
             }else{
-                int ind = lower_bound(temp.begin(), temp.end(), arr[i]) - temp.begin();
-                temp[ind] = arr[i];
+                int ind = lower_bound(tmp.begin(), tmp.end(), arr[i]) - tmp.begin();
+                tmp[ind] = arr[i];
             }
         }
 
-        return temp.size();
+        return tmp.size();
+        
     }
 };
