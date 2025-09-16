cc5777d4f484351f5c8502b2c903f4468adbec64
AngadSingh04 <singh04angad@gmail.com>
2025-09-16 10:01:33 +0530

    converting to children sum

--- 2236.cpp
+++ 2236.cpp
@@ -29,3 +29,34 @@ public:
         return checkTree(root->left) && checkTree(root->right);
     }
 };
+
+// converting
+
+class Solution {
+public:
+    void checkTree(TreeNode* root) {
+        if(root == NULL){
+            return;
+        }
+        int child = 0;
+        if(root->left){
+            child += root->left->val;
+        }
+        if(root->right){
+            child += root->right->val;
+        }
+        if(child >= root->data) root->data = child;
+        else{
+            if(root->left) root->left->data = root->data;
+            else if(root->right) root->right->data = root->data;
+        }
+
+        checkTree(root->left);
+        checkTree(root->right);
+
+        int total = 0;
+        if(root->left) total += root->left->data;
+        if(root->right) total += root->right->data;
+        if(root->left || root->right) root->data = total;
+    }
+};
