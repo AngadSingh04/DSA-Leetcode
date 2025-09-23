1fcb5c73f4563dcfec86f9cf2fa3d45550629fd5
AngadSingh04 <singh04angad@gmail.com>
2025-09-23 11:15:07 +0530

    refactor minWindow function for improved readability and performance

--- 76.cpp
+++ 76.cpp
@@ -4,32 +4,28 @@ public:
         int n = s.size();
         int m = t.size();
         vector<int> arr(256,0);
-
         for(int i = 0; i < m; i++){
             arr[t[i]]++;
         }
 
         int l = 0;
         int r = 0;
-        int cnt = 0;
-        int minLen = INT_MAX;
         int sIndex = -1;
+        int minLen = 1e9;
+        int cnt = 0;
 
         while(r < n){
-            
-            if(arr[s[r]] > 0){
-                cnt++;
-            }
+            if(arr[s[r]] > 0) cnt++;
             arr[s[r]]--;
 
             while(cnt == m){
                 if(r - l + 1 < minLen){
-                    minLen = r - l + 1;
+                    minLen = r-l+1;
                     sIndex = l;
                 }
                 arr[s[l]]++;
                 if(arr[s[l]] > 0){
-                    cnt--;
+                    cnt = cnt-1;
                 }
                 l++;
             }
